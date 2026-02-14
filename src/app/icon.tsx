c9247d5ac87f864aed162078e064e3ce30afbd08
amo95 <jameaamo@gmail.com>
2026-02-14 11:44:55 +0000

    amojames: add favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 11,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#000",
+          color: "#fff",
+          borderRadius: 6,
+          fontWeight: 700,
+          letterSpacing: -0.5,
+        }}
+      >
+        JKA
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
